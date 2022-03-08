c7fa8286774f14b411590baf9f522cd3af4b1041
Liam <brownliamm@gmail.com>
2022-03-08 09:13:53 -0500

    Add constant variables and f string equivalent to javascript basics

--- turtle/js-basics/index.js
+++ turtle/js-basics/index.js
@@ -1,6 +1,12 @@
-let human = 'Doug';
-let favColor = 'blue';
+const human = 'Doug';
+const favColor = 'blue';
+
+let numA = 7;
+let numB = 5;
 
 alert(human+' really likes the color '+favColor+'!');
+
+console.log(`The numA variable is ${numA}, and the numB variable is ${numB}`);
+console.log(`${numA} + ${numB} = ${numA+numB}`);
 
 console.log('Hello World!');
