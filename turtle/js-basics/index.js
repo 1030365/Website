40cbff30b7a68b0853433091801c5d3809b976dc
Liam <brownliamm@gmail.com>
2022-03-10 08:38:05 -0500

    Add array to js-basics

--- turtle/js-basics/index.js
+++ turtle/js-basics/index.js
@@ -3,14 +3,17 @@ let human = {
     favColor: 'blue'
 };
 
-let numA = 7;
-let numB = 5;
+let nums = [4,3];
+let hypotenuse = ((nums[0]**2)+(nums[1]**2))**0.5;
+let perimeter = nums[0]+nums[1]+hypotenuse;
+let area = nums[0]*nums[1]*0.5;
 
 alert(human.name+' really likes the color '+human.favColor+'!');
 console.log(human);
 
-console.log(`The numA variable is ${numA}, and the numB variable is ${numB}`);
-console.log(`${numA} + ${numB} = ${numA+numB}`);
+console.log(`The nums array: ${nums}`);
+console.log(`${nums[0]} + ${nums[1]} = ${nums[0]+nums[1]}`);
+console.log(`A right triangle with width ${nums[0]} and height ${nums[1]} has a hypotenuse of ${hypotenuse}, a perimeter of ${perimeter}, and an area of ${area}.`);
 
 console.log('Hello World!');
 
