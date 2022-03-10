463b9ccc251e41d6c92fdc0d830d042517eacc2d
Liam <brownliamm@gmail.com>
2022-03-10 08:54:45 -0500

    Add a function to js-basics

--- turtle/js-basics/index.js
+++ turtle/js-basics/index.js
@@ -1,16 +1,31 @@
+function triangleFinder(sides) {
+    let c = ((sides[0]**2)+(sides[1]**2))**0.5;
+    let p = sides[0]+sides[1]+c;
+    let a = sides[0]*sides[1]*0.5;
+    return [c,p,a];
+}
+
 let human = {
     name: 'Doug',
     favColor: 'blue'
 };
 
 let nums = [4,3];
-let hypotenuse = ((nums[0]**2)+(nums[1]**2))**0.5;
-let perimeter = nums[0]+nums[1]+hypotenuse;
-let area = nums[0]*nums[1]*0.5;
+let hypotenuse = triangleFinder(nums)[0];
+let perimeter = triangleFinder(nums)[1];
+let area = triangleFinder(nums)[2];
 
 alert(human.name+' really likes the color '+human.favColor+'!');
 console.log(human);
 
+console.log(`The nums array: ${nums}`);
+console.log(`${nums[0]} + ${nums[1]} = ${nums[0]+nums[1]}`);
+console.log(`A right triangle with width ${nums[0]} and height ${nums[1]} has a hypotenuse of ${hypotenuse}, a perimeter of ${perimeter}, and an area of ${area}.`);
+
+nums = [15,8];
+hypotenuse = triangleFinder(nums)[0];
+perimeter = triangleFinder(nums)[1];
+area = triangleFinder(nums)[2];
 console.log(`The nums array: ${nums}`);
 console.log(`${nums[0]} + ${nums[1]} = ${nums[0]+nums[1]}`);
 console.log(`A right triangle with width ${nums[0]} and height ${nums[1]} has a hypotenuse of ${hypotenuse}, a perimeter of ${perimeter}, and an area of ${area}.`);
