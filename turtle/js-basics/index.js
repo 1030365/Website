40fc81b094ec41bc295c641447375d71a8e19c94
Liam <brownliamm@gmail.com>
2022-03-10 08:25:29 -0500

    Make human object in js-basics

--- turtle/js-basics/index.js
+++ turtle/js-basics/index.js
@@ -1,12 +1,20 @@
-const human = 'Doug';
-const favColor = 'blue';
+let human = {
+    name: 'Doug',
+    favColor: 'blue'
+};
 
 let numA = 7;
 let numB = 5;
 
-alert(human+' really likes the color '+favColor+'!');
+alert(human.name+' really likes the color '+human.favColor+'!');
+console.log(human);
 
 console.log(`The numA variable is ${numA}, and the numB variable is ${numB}`);
 console.log(`${numA} + ${numB} = ${numA+numB}`);
 
 console.log('Hello World!');
+
+human.name = 'Luigi';
+human.favColor = 'green';
+console.log(human);
+console.log(`${human.name} really likes the color ${human.favColor}!`);
