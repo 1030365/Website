7c5a974640a595d12696b3f5f10d05e327835a6a
Liam <brownliamm@gmail.com>
2022-05-10 08:40:03 -0400

    Add winmove function to tictactoe.ts

--- browserGames/lib/tictactoe.ts
+++ browserGames/lib/tictactoe.ts
@@ -13,7 +13,7 @@ function vertical(grid) {
 function horizontal(grid) {
     let strga = []
     for (let i = 0; i < 3; i++) {
-        strga.push(grid[(3 * i): (3 * i + 3)])
+        strga.push(grid.slice((3 * i), (3 * i + 3)))
     }
     return strga
 }
@@ -45,105 +45,106 @@ function turn(grid) {
     return 2;
 }
 
-def winmove(grid):
-p = turn(grid)
-return check(grid, p)
+function winmove(grid) {
+    let p = turn(grid);
+    return check(grid,p);
+}
 
 def block(grid):
-if turn(grid) == 1:
-    p = 2
-else:
-p = 1
-return check(grid, p)
+    if turn(grid)==1:
+        p=2
+    else:
+        p=1
+    return check(grid,p)
 
-def check(grid, p):
-strg = 0
-for i in horizontal(grid):
-    if i == f'0{p}{p}':
-return 3 * strg
-if i == f'{p}0{p}':
-return 3 * strg + 1
-if i == f'{p}{p}0':
-return 3 * strg + 2
-strg += 1
-strg = 0
-for i in vertical(grid):
-    if i == f'0{p}{p}':
-return strg
-if i == f'{p}0{p}':
-return 3 + strg
-if i == f'{p}{p}0':
-return 6 + strg
-strg += 1
-strg = 0
-for i in diagonal(grid):
-    if i == f'0{p}{p}':
-return 2 * strg
-if i == f'{p}0{p}':
-return 4
-if i == f'{p}{p}0':
-return 8 - (2 * strg)
-strg += 1
-return '?'
+def check(grid,p):
+    strg=0
+    for i in horizontal(grid):
+        if i==f'0{p}{p}':
+            return 3*strg
+        if i==f'{p}0{p}':
+            return 3*strg+1
+        if i==f'{p}{p}0':
+            return 3*strg+2
+        strg+=1
+    strg=0
+    for i in vertical(grid):
+        if i==f'0{p}{p}':
+            return strg
+        if i==f'{p}0{p}':
+            return 3+strg
+        if i==f'{p}{p}0':
+            return 6+strg
+        strg+=1
+    strg=0
+    for i in diagonal(grid):
+        if i==f'0{p}{p}':
+            return 2*strg
+        if i==f'{p}0{p}':
+            return 4
+        if i==f'{p}{p}0':
+            return 8-(2*strg)
+        strg+=1
+    return '?'
 
 def move(grid):
-if winmove(grid) == '?':
-    if block == '?':
-        return '?'
-return block(grid)
-return winmove(grid)
+    if winmove(grid)=='?':
+        if block=='?':
+            return '?'
+        return block(grid)
+    return winmove(grid)
 
 def options(grid):
-if move(grid) == '?':
-    strg = []
-for i in range(0, 9):
-    if grid[i] == '0':
-        strg.append(i)
-return strg
-return [move(grid)]
+    if move(grid)=='?':
+        strg=[]
+        for i in range(0,9):
+            if grid[i]=='0':
+                strg.append(i)
+        return strg
+    return [move(grid)]
 
 def play(grid):
-strga = options(grid)
-if len(strga) == 1:
-    choice = strga[0]
-else:
-strgb = len(strga)
-strgc = 'choose to play at '
-for i in range(0, strgb):
-    if strgb - i == 1:
-        if strgb == 2:
-            strgc += ' '
-strgc += f'or {strga[-1]+1}:'
-break
-strgc += str(strga[i] + 1)
-if strgb > 2:
-    strgc += ', '
-#while True:
-#choice = int(input(strgc)) - 1
-#if choice in strga:
-#break
-#print('invalid choice')
-choice = strga[randint(0, strgb - 1)]
-return grid[: choice]+str(turn(grid)) + grid[(choice + 1):]
+    strga=options(grid)
+    if len(strga)==1:
+        choice=strga[0]
+    else:
+        strgb=len(strga)
+        strgc='choose to play at '
+        for i in range(0,strgb):
+            if strgb-i==1:
+                if strgb==2:
+                    strgc+=' '
+                strgc+=f'or {strga[-1]+1}:'
+                break
+            strgc+=str(strga[i]+1)
+            if strgb>2:
+                strgc+=', '
+        #while True:
+            #choice=int(input(strgc))-1
+            #if choice in strga:
+                #break
+            #print('invalid choice')
+        choice=strga[randint(0,strgb-1)]
+    return grid[:choice]+str(turn(grid))+grid[(choice+1):]
 
 
 
 
-board = '000000000'
+board='000000000'
 
-winner = 0
+winner=0
 
-for i in range(0, 9):
+for i in range(0,9):
     display(board)
-print('_____')
-if winmove(board) == '?':
-    board = play(board)
-else:
-winner = turn(board)
-board = play(board)
-break
+    print('_____')
+    if winmove(board)=='?':
+        board=play(board)
+    else:
+        winner=turn(board)
+        board=play(board)
+        break
 display(board)
-if winner == 0:
+if winner==0:
     print('it\'s a tie')
 else:
-print(f'player {winner} wins!')
+    print(f'player {winner} wins!')
