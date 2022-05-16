af742a579b0bcdcdadefe982f7bbdcc7239960cf
Liam <brownliamm@gmail.com>
2022-05-16 09:01:39 -0400

    Add boundaries to robots.ts

--- browserGames/dist/robots.js
+++ browserGames/dist/robots.js
@@ -5,19 +5,32 @@ let canvas = document.getElementById('myGame');
 let ctx = canvas.getContext("2d");
 
 function check(e) {
-  console.log(e.keyCode);
   let dx = 0;
   let dy = 0;
 
   if (e.keyCode >= 37 && e.keyCode <= 40) {
     dx = [-1, 0, 1, 0][e.keyCode - 37];
     dy = [0, -1, 0, 1][e.keyCode - 37];
+
+    if (10 * (x + dx) < canvas.width && 10 * (y + dy) < canvas.height && 10 * (x + dx) >= 0 && 10 * (y + dy) >= 0) {
+      move(dx, dy);
+    }
   }
 
   console.log(`move by: (${dx}, ${dy})`);
 }
 
-function game() {
+function move(newX, newY) {
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(x * 10, y * 10, 10, 10);
+  x += newX;
+  y += newY;
   ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0, 0, 150, 75);
+  ctx.fillRect(x * 10, y * 10, 10, 10);
 }
+
+function game() {
+  move(0, 0);
+}
+
+game();
--- browserGames/lib/robots.ts
+++ browserGames/lib/robots.ts
@@ -7,17 +7,29 @@ let canvas = document.getElementById('myGame');
 let ctx = canvas.getContext("2d");
 
 function check(e) {
-    console.log(e.keyCode)
     let dx = 0
     let dy = 0
     if (e.keyCode >= 37 && e.keyCode <= 40) {
         dx = [-1,0,1,0][e.keyCode-37]
         dy = [0,-1,0,1][e.keyCode-37]
+        if (10*(x+dx) < canvas.width && 10*(y+dy) < canvas.height && 10*(x+dx) >= 0 && 10*(y+dy) >= 0) { 
+            move(dx, dy);
+        }
     }
     console.log(`move by: (${dx}, ${dy})`)
 }
 
-function game() {
-    ctx.fillStyle = "#FF0000"
-    ctx.fillRect(0, 0, 150, 75);
+function move(newX, newY) {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(x*10, y*10, 10, 10);
+    x += newX;
+    y += newY;
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(x*10, y*10, 10, 10);
 }
+
+function game() {
+    move(0,0)
+}
+
+game()
