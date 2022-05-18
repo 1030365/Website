8bd0802836a8854c0732ad076b820299d043c31c
Liam <brownliamm@gmail.com>
2022-05-18 08:34:48 -0400

    Add safely_place_player to robots.ts

--- browserGames/dist/robots.js
+++ browserGames/dist/robots.js
@@ -1,11 +1,44 @@
-window.addEventListener('keydown', this.check, false);
-let x = 15;
-let y = 10;
+window.addEventListener('keydown', check, false);
+
+class Player {
+  constructor(x, y) {
+    this.x = 1;
+    this.y = 2;
+  }
+
+}
+
+;
+
+class Robot {
+  constructor(x, y) {
+    this.x = 1;
+    this.y = 2;
+  }
+
+}
+
+let player = new Player(1, 1);
 let canvas = document.getElementById('myGame');
 let ctx = canvas.getContext("2d");
 
-function radnint(a, b) {
+function randint(a, b) {
   return Math.floor(a + (b - a + 1) * Math.random());
+}
+
+function place_player() {
+  player.x = randint(0, 63);
+  player.y = randint(0, 47);
+}
+
+function safely_place_player() {
+  place_player();
+
+  while (collided(player, robots)) {
+    place_player();
+  }
+
+  teleport(10 * player.x + 5, 10 * player.y - 5);
 }
 
 function check(e) {
@@ -16,7 +49,7 @@ function check(e) {
     dx = [-1, 0, 1, 0][e.keyCode - 37];
     dy = [0, -1, 0, 1][e.keyCode - 37];
 
-    if (10 * (x + dx) < canvas.width && 10 * (y + dy) < canvas.height && 10 * (x + dx) >= 0 && 10 * (y + dy) >= 0) {
+    if (10 * (player.x + dx) < canvas.width && 10 * (player.y + dy) < canvas.height && 10 * (player.x + dx) >= 0 && 10 * (player.y + dy) >= 0) {
       move(dx, dy);
     }
   }
@@ -24,29 +57,26 @@ function check(e) {
   console.log(`move by: (${dx}, ${dy})`);
 }
 
-class Player {
-  constructor() {
-    x += 0;
-  }
-
-}
-
-class Robot {
-  constructor() {
-    x += 0;
-  }
-
-}
-
 function move(newX, newY) {
   ctx.fillStyle = "#FFFFFF";
-  ctx.fillRect(x * 10, y * 10, 10, 10);
-  x += newX;
-  y += newY;
+  ctx.fillRect(player.x * 10, player.y * 10, 10, 10);
+  player.x += newX;
+  player.y += newY;
   ctx.fillStyle = "#000000";
-  ctx.fillRect(x * 10, y * 10, 10, 10);
+  ctx.fillRect(player.x * 10, player.y * 10, 10, 10);
   ctx.fillStyle = "#FFFFFF";
-  ctx.fillRect(x * 10 + 1, y * 10 + 1, 8, 8);
+  ctx.fillRect(player.x * 10 + 1, player.y * 10 + 1, 8, 8);
+}
+
+function teleport(newX, newY) {
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(player.x * 10, player.y * 10, 10, 10);
+  player.x = newX;
+  player.y = newY;
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(player.x * 10, player.y * 10, 10, 10);
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(player.x * 10 + 1, player.y * 10 + 1, 8, 8);
 }
 
 function game() {
@@ -54,3 +84,4 @@ function game() {
 }
 
 game();
+export default game;
--- browserGames/lib/robots.ts
+++ browserGames/lib/robots.ts
@@ -1,13 +1,43 @@
-window.addEventListener('keydown',this.check,false);
+window.addEventListener('keydown',check,false);
 
-let x = 15
-let y = 10
+class Player {
+    x: number;
+    y: number;
+    constructor(x, y) {
+        this.x = 1;
+        this.y = 2;
+    }
+};
 
-let canvas = document.getElementById('myGame');
+class Robot {
+    x: number;
+    y: number;
+    constructor(x, y) {
+        this.x = 1;
+        this.y = 2;
+    }
+}
+
+let player = new Player(1, 1);
+
+let canvas = document.getElementById('myGame') as HTMLCanvasElement;
 let ctx = canvas.getContext("2d");
 
-function radnint(a, b) {
+function randint(a, b) {
     return Math.floor(a+(b-a+1)*Math.random())
+}
+
+function place_player() {
+    player.x = randint(0,63);
+    player.y = randint(0,47);
+}
+
+function safely_place_player() {
+    place_player()
+    while (collided(player,robots)) {
+        place_player()
+    }
+    teleport(10*player.x+5,10*player.y-5)
 }
 
 function check(e) {
@@ -16,34 +46,35 @@ function check(e) {
     if (e.keyCode >= 37 && e.keyCode <= 40) {
         dx = [-1,0,1,0][e.keyCode-37]
         dy = [0,-1,0,1][e.keyCode-37]
-        if (10*(x+dx) < canvas.width && 10*(y+dy) < canvas.height && 10*(x+dx) >= 0 && 10*(y+dy) >= 0) {
+        if (10*(player.x+dx) < canvas.width && 10*(player.y+dy) < canvas.height && 10*(player.x+dx) >= 0 && 10*(player.y+dy) >= 0) {
             move(dx, dy);
         }
     }
     console.log(`move by: (${dx}, ${dy})`)
 }
 
-class Player {
-    constructor() {
-        x += 0;
-    }
-}
 
-class Robot {
-    constructor() {
-        x += 0;
-    }
-}
 
 function move(newX, newY) {
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(x*10, y*10, 10, 10);
-    x += newX;
-    y += newY;
+    ctx.fillRect(player.x*10, player.y*10, 10, 10);
+    player.x += newX;
+    player.y += newY;
     ctx.fillStyle = "#000000";
-    ctx.fillRect(x*10, y*10, 10, 10);
+    ctx.fillRect(player.x*10, player.y*10, 10, 10);
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(x*10+1, y*10+1, 8, 8);
+    ctx.fillRect(player.x*10+1, player.y*10+1, 8, 8);
+}
+
+function teleport(newX, newY) {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(player.x*10, player.y*10, 10, 10);
+    player.x = newX;
+    player.y = newY;
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(player.x*10, player.y*10, 10, 10);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(player.x*10+1, player.y*10+1, 8, 8);
 }
 
 function game() {
@@ -51,3 +82,5 @@ function game() {
 }
 
 game()
+
+export default game;
